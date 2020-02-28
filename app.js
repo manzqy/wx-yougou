8e97eb1a369c82ca8ce5485f7c1b1ac543831f7d
xiaoYu <18839199638@163.com>
2020-02-28 20:27:44 +0800

    首页样式完成

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import request from './utils/request'
 App({
   //onLaunch,onShow: options(path,query,scene,shareTicket,referrerInfo(appId,extraData))
   onLaunch: function(options){
-    request.defaults.baseURL = 'https://api-hmugo-web.itheima.net/api/public/v1/'
+    request.defaults.baseURL = 'https://api-hmugo-web.itheima.net/api/public/v1'
     request.onError(() => {
       console.log('%c请求错误', 'color:green;font-size:25px;')
     })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,37 +1,77 @@
+import request from '../../utils/request'
 Page({
   data: {
-    
+    swiperData: [],
+    menusData: [],
+    floorData: [],
+    isShow: false
   },
   //options(Object)
-  onLoad: function(options){
-    
+  onLoad: function(options) {
+    this.getSwiperData()
+    this.getMenuData()
+    this.getFloorData()
   },
-  onReady: function(){
-    
+  // 轮播图数据
+  getSwiperData() {
+    request({
+      url: '/home/swiperdata'
+    }).then(({ data }) => {
+      const { message } = data
+      this.setData({
+        swiperData: message
+      })
+    })
   },
-  onShow: function(){
-    
+  // 菜单数据
+  getMenuData() {
+    request({
+      url: '/home/catitems'
+    }).then(({ data }) => {
+      const { message } = data
+      this.setData({
+        menusData: message
+      })
+    })
   },
-  onHide: function(){
-
+  // 得到楼层数据
+  getFloorData() {
+    request({
+      url: '/home/floordata'
+    }).then(({ data }) => {
+      const { message } = data
+      this.setData({
+        floorData: message
+      })
+    })
   },
-  onUnload: function(){
-
+  toScrollTop() {
+    wx.pageScrollTo({
+      scrollTop: 0,
+      duration: 300
+    })
   },
-  onPullDownRefresh: function(){
-
+  onPageScroll: function(e) {
+    const { isShow } = this.data
+    if (e.scrollTop > 100 && !isShow) {
+      this.setData({
+        isShow: !isShow
+      })
+      return
+    }
+    if (e.scrollTop < 100 && isShow) {
+      this.setData({
+        isShow: !isShow
+      })
+    }
   },
-  onReachBottom: function(){
-
-  },
-  onShareAppMessage: function(){
-
-  },
-  onPageScroll: function(){
-
-  },
+  onReady: function() {},
+  onShow: function() {},
+  onHide: function() {},
+  onUnload: function() {},
+  onPullDownRefresh: function() {},
+  onReachBottom: function() {},
+  onShareAppMessage: function() {},
   //item(index,pagePath,text)
-  onTabItemTap:function(item){
-
-  }
+  onTabItemTap: function(item) {}
 })
