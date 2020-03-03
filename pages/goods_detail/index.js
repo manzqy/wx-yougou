0e42fe7142503ea70716b795a3097668a53d7b5e
xiaoYu <18839199638@163.com>
2020-03-03 21:32:42 +0800

    详情页完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,3 +1,4 @@
+import request from '../../utils/request'
 // pages/goods_detail/index.js
 Page({
 
@@ -5,16 +6,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    goods_id: null,
+    detailData: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options)
+    const {goods_id} = options
+    this.setData({
+      goods_id
+    })
+    this.getDetailData()
   },
-
+  // 拿到数据
+  getDetailData() {
+    request({
+      url: '/goods/detail',
+      data: {
+        goods_id: this.data.goods_id
+      }
+    }).then(({data}) => {
+      console.log(data.message)
+      this.setData({
+        detailData: data.message
+      })
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
